7d1e02a5537f1be4db7a9d37144352499e04a1e5
William Owen <wowen14@gmail.com>
2026-09-05 20:18:17 -0500

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE = 'perinatal-loss-keepsake-v2-4-pwa';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app-part1.js',
+  './app-part2.js',
+  './app-part3.js',
+  './app-part4.js',
+  './manifest.json',
+  './committee-logo.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (response && response.status === 200 && response.type === 'basic') {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      });
+    })
+  );
+});
